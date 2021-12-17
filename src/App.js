1141eaae6fc06c942e11092cc997238ee53dea80
AlexMarquesAlves <atanaelleonardo@gmail.com>
2021-12-17 03:05:46 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React, { useEffect, useState } from "react";
-import './App.css';
-import Tmdb from './Tmdb';
-import MovieRow from './components/MovieRow';
-import FeaturedMovie from './components/FeaturedMovie';
-import Header from './components/Header';
-
-
+import "./App.css";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
@@ -19,12 +18,14 @@ export default () => {
       setMovieList(list);
 
       // pegando o Featured
-      let originals = list.filter(i => i.slug === "originals");
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       let chosen = originals[0].items.results[randomChosen];
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
       setfeaturedData(chosenInfo);
-    }
+    };
 
     loadAll();
   }, []);
@@ -34,24 +35,21 @@ export default () => {
       if (window.scrollY > 40) {
         setBlackHeader(true);
       } else {
-        setBlackHeader(false)
+        setBlackHeader(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', scrollListener);
+    window.addEventListener("scroll", scrollListener);
     return () => {
-      window.removeEventListener('scroll', scrollListener);
-    }
+      window.removeEventListener("scroll", scrollListener);
+    };
   }, []);
 
   return (
     <div className="page">
-
       <Header black={blackHeader} />
 
-      {featuredData &&
-        <FeaturedMovie item={featuredData} />
-      }
+      {featuredData && <FeaturedMovie item={featuredData} />}
 
       <section className="lists">
         {movieList.map((item, key) => (
@@ -59,16 +57,24 @@ export default () => {
         ))}
       </section>
       <footer>
-        Feito com <span role="img" aria-label="heart">❤️</span> por <strong>Leonardo Marquês</strong><br />
+        Feito com{" "}
+        <span role="img" aria-label="heart">
+          ❤️
+        </span>{" "}
+        por <strong>Leonardo Marquês</strong>
+        <br />
         Direitos de imagem para Netflix <br />
         Dados pegos do site Themoviedb.org
       </footer>
 
-      {movieList.length <= 0 &&
+      {movieList.length <= 0 && (
         <div className="loading">
-          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+          <img
+            src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+            alt="Carregando"
+          />
         </div>
-      }
+      )}
     </div>
   );
-}
+};
